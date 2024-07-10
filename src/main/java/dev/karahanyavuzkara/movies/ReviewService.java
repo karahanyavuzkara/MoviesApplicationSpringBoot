0c2ef56f354af716d1a28afb1755bf610f38c7c2
nefretac <karahanyavuz61@gmail.com>
2024-07-10 23:16:18 +0300

    endpoints

--- src/main/java/dev/karahanyavuzkara/movies/ReviewService.java
+++ src/main/java/dev/karahanyavuzkara/movies/ReviewService.java
@@ -14,12 +14,13 @@ public class ReviewService {
     @Autowired
     private MongoTemplate mongoTemplate;
     public Review createReview(String reviewBody, String imdbId){
-        Review review = new Review(reviewBody);
-        reviewRepository.insert(review);
+        Review review = reviewRepository.insert(new Review(reviewBody));
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewIds").value(review));
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
 
+        return review;
     }
 }
