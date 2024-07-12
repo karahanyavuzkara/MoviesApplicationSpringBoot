8f683b9874cf52a644ae05bb79a1831078294319
nefretac <karahanyavuz61@gmail.com>
2024-07-12 16:46:29 +0300

    endpoints

--- src/main/java/dev/karahanyavuzkara/movies/MovieController.java
+++ src/main/java/dev/karahanyavuzkara/movies/MovieController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
