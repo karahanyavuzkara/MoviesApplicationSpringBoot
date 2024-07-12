95255bb791325b1116a1f6f6c56021c39a695699
nefretac <karahanyavuz61@gmail.com>
2024-07-12 17:01:19 +0300

    endpoints

--- src/main/java/dev/karahanyavuzkara/movies/MovieRepository.java
+++ src/main/java/dev/karahanyavuzkara/movies/MovieRepository.java
@@ -1,14 +1,13 @@
 package dev.karahanyavuzkara.movies;
 
 import org.bson.types.ObjectId;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@RestController
+@Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
     Optional<Movie> findMovieByImdbId(String imdbId);
 }
+
